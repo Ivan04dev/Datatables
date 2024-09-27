a5d04b926feed58db2558c5eb468afb9eac4acdc
ivdev04 <101958046+Ivan04dev@users.noreply.github.com>
2024-09-27 10:07:17 -0600

    Create configuracion_datatables.js
    Archivo de configuración para datatables en español

---
+++ configuracion_datatables.js
@@ -0,0 +1,28 @@
+<script>
+
+$(document).ready(function() {
+    $("#tabla-procesos").DataTable({
+        language: {
+            "decimal": ",",
+            "thousands": ".",
+            "lengthMenu": "Mostrar _MENU_ registros",
+            "zeroRecords": "No se encontraron resultados",
+            "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+            "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+            "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+            "search": "Buscar:",
+            "paginate": {
+                "first": "Primero",
+                "last": "Último",
+                "next": "Siguiente",
+                "previous": "Anterior"
+            },
+            "aria": {
+                "sortAscending": ": Activar para ordenar la columna de manera ascendente",
+                "sortDescending": ": Activar para ordenar la columna de manera descendente"
+            }
+        }
+    });
+});
+
+</script>
